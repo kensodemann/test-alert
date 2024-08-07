f6d42f55e7fff9a376927c1a39106626817d019b
Ken Sodemann <ken@ionic.io>
2024-08-07 09:02:21 -0500

    implement the alert

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { AlertController, IonButton, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private alertController: AlertController) {}
+
+  async showAlert() {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Are you sure you want to do this?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          htmlAttributes: {
+            'aria-label': 'No way dude',
+          },
+        },
+        {
+          text: 'Yes',
+          role: 'confirm',
+          htmlAttributes: {
+            'aria-label': 'You bet',
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
 }
